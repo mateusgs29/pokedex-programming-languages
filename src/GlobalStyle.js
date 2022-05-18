6d8a0f01efdd0238140fe98553792647e27e955b
Mateus Gomes <mateus.gs1999@gmail.com>
2022-05-18 10:59:43 -0300

    estilo global

---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Signika Negative', sans-serif;
+    text-decoration: none;
+  }
+
+  body {
+    background-color: #202020;
+  }
+`
